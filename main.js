c9428db00fada9dea646e155ddca7f4235b2e5db
Amanuel <amtadesse@excellerentsolutions.com>
2022-05-19 12:36:37 -0400

    style binding

---
+++ main.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            product: {
+                name: 'Socks',
+                description: 'A pair of warm, fuzzy socks',
+                image: './assets/images/socks_green.jpg',
+                inStock: true,
+                inventory: 6,
+                onSale: true,
+                details: ["50% cotton", "30% polyester", "20% rayon", ""],
+                variants: [
+                    { variantId: 2234, color: "green",image: './assets/images/socks_green.jpg', quantity: 0 },
+                    { variantId: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 10 }
+                ]
+            }
+        }
+    }, 
+    methods:{
+        updateImage(variantImage) {
+            this.product.image = variantImage
+        }
+    }
+}).mount("#app")
