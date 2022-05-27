7e9af512090555cb615d286e950ad452c966d7bc
Amanuel <amtadesse@excellerentsolutions.com>
2022-05-27 10:48:45 -0400

    computed properties added

--- main.js
+++ main.js
@@ -4,26 +4,38 @@ createApp({
     data() {
         return {
             product: {
+                brand: 'Adidas',
                 name: 'Socks',
                 description: 'A pair of warm, fuzzy socks',
-                image: './assets/images/socks_green.jpg',
-                inStock: true,
+                selectedVariant: 0,
                 inventory: 6,
                 onSale: true,
                 details: ["50% cotton", "30% polyester", "20% rayon", ""],
                 variants: [
-                    { variantId: 2234, color: "green",image: './assets/images/socks_green.jpg', quantity: 0 },
-                    { variantId: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 10 }
+                    { variantId: 2234, color: "green", image: './assets/images/socks_green.jpg', quantity: 50 },
+                    { variantId: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 0 }
                 ]
             }
         }
-    }, 
-    methods:{
-        updateImage(variantImage) {
-            this.product.image = variantImage
+    },
+    methods: {
+        updateVariant(index) {
+            this.product.selectedVariant = index
         },
         addToCart(variantId) {
 
         }
+    },
+    computed: {
+        title() {
+            return this.product.brand + ' ' + this.product.name
+        },
+        image() {
+            return this.product.variants[this.product.selectedVariant].image
+        },
+        inStock() {
+            return this.product.variants[this.product.selectedVariant].quantity
+        },
+       
     }
 }).mount("#app")
