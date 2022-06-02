c91f22fe29255f222dbeadf31ac5ea3df6188569
Amanuel <amtadesse@excellerentsolutions.com>
2022-06-02 04:38:52 -0400

    component and prop added

--- main.js
+++ main.js
@@ -1,41 +1,17 @@
-const { createApp } = Vue
-
-createApp({
+const app = Vue.createApp({
     data() {
         return {
-            product: {
-                brand: 'Adidas',
-                name: 'Socks',
-                description: 'A pair of warm, fuzzy socks',
-                selectedVariant: 0,
-                inventory: 6,
-                onSale: true,
-                details: ["50% cotton", "30% polyester", "20% rayon", ""],
-                variants: [
-                    { variantId: 2234, color: "green", image: './assets/images/socks_green.jpg', quantity: 50 },
-                    { variantId: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 0 }
-                ]
-            }
+            cart:0,
+            premium:false,
         }
     },
     methods: {
-        updateVariant(index) {
-            this.product.selectedVariant = index
+        addToCart() {
+            this.cart += 1
         },
-        addToCart(variantId) {
-
+        updateVariant(index) {
+            this.selectedVariant = index
         }
     },
-    computed: {
-        title() {
-            return this.product.brand + ' ' + this.product.name
-        },
-        image() {
-            return this.product.variants[this.product.selectedVariant].image
-        },
-        inStock() {
-            return this.product.variants[this.product.selectedVariant].quantity
-        },
-       
-    }
-}).mount("#app")
+    
+})
